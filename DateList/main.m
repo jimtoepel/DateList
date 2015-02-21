79b1f4c9df219f936b404efab23864be557f5af2
jimtoepel <jim.toepel@gmail.com>
2015-02-20 20:13:01 -0800

    Initial Commit

---
+++ DateList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DateList
+//
+//  Created by Jim Toepel on 2/20/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
