20d1c7d45c9f0aa088da320fd358eb1dfd9cc994
jimtoepel <jim.toepel@gmail.com>
2015-02-21 15:01:54 -0800

    Added stuff to Mutable arrays.

--- DateList/main.m
+++ DateList/main.m
@@ -16,14 +16,25 @@ int main(int argc, const char * argv[]) {
         NSDate *tomorrow = [now dateByAddingTimeInterval:24.0 * 60 *60];
         NSDate *yesterday = [now dateByAddingTimeInterval:-24 * 60 * 60];
         
-        // Create and Array with all three dates
-        NSArray *dateList = @[now, tomorrow, yesterday];
+        // Create a mutable array
+        NSMutableArray *dateList = [NSMutableArray array];
+        
+        // Add two dates to the array
+        [dateList addObject:now];
+        [dateList addObject:tomorrow];
+        
+        // Add yesterday to the beginning of the list
+        [dateList insertObject:yesterday atIndex:0];
         
         
         // iterate over the array
         for(NSDate *d in dateList) {
             NSLog(@"Here is a date: %@", d);
         }
+        
+        // remove yesterday
+        [dateList removeObjectAtIndex:0];
+        NSLog(@"Now the first date is %@", dateList[0]);
         
         NSLog(@"Hello, World!");
     }
